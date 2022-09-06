8f44c189bab9aa8ffe4879fcbdb4bd5eefcb27ac
SuhanaNadeem <suhana.nadeemv@gmail.com>
2022-09-06 15:07:51 -0400

    edited dashboard latest transaction ui

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -117,7 +117,7 @@ const Home: NextPage<HomeProps> = ({ locations, skus, user }: HomeProps) => {
                       </button>
                     </Link>
                   </div>
-                  <div className=" h-px bg-white mb-2 w-full" />
+                  <div className="h-px bg-white mb-2 w-full" />
                   <div className="flex flex-col w-full">
                     {separateByLocationId(user.transactions[0].orders).map(
                       (arr, index) => (
@@ -134,7 +134,7 @@ const Home: NextPage<HomeProps> = ({ locations, skus, user }: HomeProps) => {
                           </div>
                           {arr.map((o, i) => (
                             <div
-                              className="flex ml-6 justify-between"
+                              className="flex justify-between"
                               key={o.id + "items"}
                             >
                               <div
@@ -149,45 +149,48 @@ const Home: NextPage<HomeProps> = ({ locations, skus, user }: HomeProps) => {
                                   alt={skuName(o.sku)}
                                   className="rounded"
                                 />
-                                <div className="flex-col text-start ml-2">
+                                <div className="flex-col text-start ml-4">
                                   <div className="text-base font-theinhardt">
                                     {o.sku.product.name}
                                   </div>
-                                  <div className=" text-xs font-theinhardt-300">
+                                  <div className="text-sm font-theinhardt-300">
                                     {`${o.sku.size} | ${o.sku.materialShort}`}
                                   </div>
                                 </div>
-                                <div className="ml-2 text-sm font-theinhardt-300">{`x ${o.quantity}`}</div>
                               </div>
+                                <div className="flex items-center font-theinhardt-300 text-md">{`x ${o.quantity}`}</div>
                               <div className="flex mb-3 items-center">
                                 <div className="flex-col justify-center text-center">
-                                  <div className="text-base font-theinhardt">
-                                    {o.status.toLowerCase()}
+                                  <div className="font-theinhardt uppercase tracking-wide text-xs text-re-green-600">
+                                    {o.status}
                                   </div>
                                   <div className=" text-xs font-theinhardt-300">
                                     {`Est. shipping ${addDays(
                                       o.createdAt,
                                       7
                                     ).toLocaleDateString("en-us", {
-                                      month: "long",
+                                      month: "short",
                                       day: "numeric",
                                     })}`}
                                   </div>
                                 </div>
-                                <div className="flex flex-col ml-2 justify-between">
+                                <div className="flex items-center mx-6">
                                   <Link
                                     href={{
                                       pathname: "/dashboard/checkout",
                                       query: { orderId: o.id },
                                     }}
                                   >
-                                    <button className="px-2 py-0.5 bg-re-gray-400 rounded text-xs mb-1 hover:bg-re-green-600 hover:text-black">
+                                    <button className="px-3 py-2 bg-re-gray-400 rounded-10 text-xs hover:bg-re-green-600 hover:text-black">
                                       Re-order
                                     </button>
                                   </Link>
+                                </div>
+                                <div className="flex items-center">
                                   <button
-                                    className="px-2 py-0.5 bg-re-gray-400 rounded text-xs hover:bg-opacity-60"
+                                    className="flex items-center flex-col justify-center px-3 py-2 bg-re-gray-400 rounded-10 text-xs bg-opacity-40"
                                     onClick={() => make20Ids()}
+                                    disabled
                                   >
                                     Schedule
                                   </button>
