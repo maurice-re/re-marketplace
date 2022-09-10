c3ce1f28faae63bd88c2fa11a9cd120760674aef
maurice-re <107324227+maurice-re@users.noreply.github.com>
2022-09-09 21:34:49 -0400

    Update reset-all.ts

--- pages/api/database/reset-all.ts
+++ pages/api/database/reset-all.ts
@@ -4,9 +4,7 @@ import prisma from "../../../constants/prisma";
 
 export default async function resetAll(req: Request, res: Response) {
   await prisma.account.deleteMany({});
-  await prisma.apiLogging.deleteMany({});
   await prisma.company.deleteMany({});
-  await prisma.event.deleteMany({});
   await prisma.image.deleteMany({});
   await prisma.location.deleteMany({});
   await prisma.order.deleteMany({});
@@ -14,7 +12,6 @@ export default async function resetAll(req: Request, res: Response) {
   await prisma.session.deleteMany({});
   await prisma.sku.deleteMany({});
   await prisma.transaction.deleteMany({});
-  await prisma.untrackedInventory.deleteMany({});
   await prisma.user.deleteMany({});
   await prisma.verificationToken.deleteMany({});
   res.status(200).send
