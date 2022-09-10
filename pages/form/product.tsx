7a9769790675fdfe6725c1819b93cda9d90085c0
maurice-re <107324227+maurice-re@users.noreply.github.com>
2022-09-09 22:22:24 -0400

    load product catalog on product page

--- pages/form/product.tsx
+++ pages/form/product.tsx
@@ -5,14 +5,19 @@ import { useRouter } from "next/router";
 import ProductPage from "../../components/form/product-page";
 import ProgressBar from "../../components/form/progress-bar";
 import ReLogo from "../../components/form/re-logo";
+import prisma from "../../constants/prisma";
 import { useFormState } from "../../context/form-context";
 
-const Product: NextPage = () => {
+type ProductProps = {
+  _products: Product[];
+};
+
+const Product: NextPage<ProductProps> = ({ _products }) => {
   const router = useRouter();
   const { id } = router.query;
   const productId = id == "swapcup" ? "SC1" : "SB2";
   const { productCatalog } = useFormState();
-  const product: Product = productCatalog.find((p) => productId == p.id)!;
+  const product: Product = _products.find((p) => productId == p.id)!;
 
   return (
     <div className="w-screen h-screen bg-black flex">
@@ -29,3 +34,13 @@ const Product: NextPage = () => {
 };
 
 export default Product;
+
+export async function getStaticProps() {
+  const products = await prisma.product.findMany();
+
+  return {
+    props: {
+      _products: JSON.parse(JSON.stringify(products)),
+    },
+  };
+}
