9b41e9477b5b46cb8186e33efcc9c275d60be18b
maurice-re <107324227+maurice-re@users.noreply.github.com>
2022-09-09 20:48:50 -0400

    remove react query usage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,9 @@
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
-import { useState } from "react";
-import { QueryClient } from "react-query";
 import { FormStateProvider } from "../context/form-context";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [queryClient] = useState<QueryClient>(() => new QueryClient());
   return (
     <SessionProvider session={pageProps.session}>
       <FormStateProvider>
