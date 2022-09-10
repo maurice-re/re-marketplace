1b887e0521bbac80404b69988f8671b786cf250e
maurice-re <107324227+maurice-re@users.noreply.github.com>
2022-09-09 20:28:04 -0400

    remove unused react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { useState } from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient } from "react-query";
 import { FormStateProvider } from "../context/form-context";
 import "../styles/globals.css";
 
@@ -9,11 +9,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState<QueryClient>(() => new QueryClient());
   return (
     <SessionProvider session={pageProps.session}>
-      <QueryClientProvider client={queryClient}>
-        <FormStateProvider>
-          <Component {...pageProps} />
-        </FormStateProvider>
-      </QueryClientProvider>
+      <FormStateProvider>
+        <Component {...pageProps} />
+      </FormStateProvider>
     </SessionProvider>
   );
 }
