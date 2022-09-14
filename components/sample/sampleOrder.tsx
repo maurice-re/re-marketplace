425a835d9daaf1ae60a160438bc729dccc5350f8
SuhanaNadeem <suhana.nadeemv@gmail.com>
2022-09-14 11:21:19 -0400

    add skus to /sample using quickOrder as template for sampleOrder

---
+++ components/sample/sampleOrder.tsx
@@ -0,0 +1,99 @@
+import Image from "next/image";
+import { useState } from "react";
+import { skuName, SkuProduct } from "../../utils/dashboard/dashboardUtils";
+
+function SampleOrder({ skus }: { skus: SkuProduct[] }) {
+  const [selected, setSelected] = useState<SkuProduct[]>([]);
+  const [skuIdQuantity, setSkuIdQuantity] = useState<[SkuProduct, string][]>(
+    []
+  );
+
+  function handleItemPress(skuSelected: SkuProduct) {
+    const isSelected = selected.find((s) => s.id == skuSelected.id);
+    if (isSelected) {
+      setSelected((prev) => prev.filter((s) => s.id != skuSelected.id));
+      setSkuIdQuantity((prev) => prev.filter((s) => s[0].id != skuSelected.id));
+    } else {
+      setSelected((prev) => [...prev, skuSelected]);
+      setSkuIdQuantity((prev) => [...prev, [skuSelected, "0"]]);
+    }
+  }
+
+  function handleQuantityChange(val: string, skuSelected: SkuProduct) {
+    setSkuIdQuantity((prev) => {
+      return prev.map((tup) => {
+        if (tup[0].id == skuSelected.id) {
+          tup[1] = val;
+        }
+        return tup;
+      });
+    });
+  }
+
+  return (
+    <div className="flex flex-col mx-1 px-4 py-4 bg-re-gray-500 bg-opacity-70 rounded-2xl items-start">
+      <h1 className=" text-re-green-500 font-theinhardt text-2xl mb-2">
+        Quick Order
+      </h1>
+      <div className=" h-px bg-white mb-4 w-full" />
+      <div className="flex justify-between w-full gap-4">
+        <div
+          className={`grid gap-4 h-96 overflow-y-scroll w-full pr-1 items-start ${
+            selected.length == 0
+              ? "2xl:grid-cols-7 xl:grid-cols-6 lg:grid-cols-5 grid-cols-4"
+              : "2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 grid-cols-2"
+          }`}
+        >
+          {skus.map((sku) => (
+            <div
+              key={sku.id}
+              className="flex flex-col items-center mx-1 mb-2 group"
+            >
+              <button
+                className={`rounded w-24 h-24 group-hover:border-re-green-500 group-hover:border-2 group-active:border-re-green-700 border-white ${
+                  selected.includes(sku)
+                    ? "border-re-green-600 border-3"
+                    : "border"
+                }`}
+                onClick={() => handleItemPress(sku)}
+              >
+                <Image
+                  src={sku.mainImage}
+                  height={96}
+                  width={96}
+                  alt={skuName(sku)}
+                />
+              </button>
+              <div className="flex text-xs text-center mt-1">
+                <div>{sku.size}</div>
+                <div className="w-1" />
+                <div>|</div>
+                <div className="w-1" />
+                <div>{sku.materialShort}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+        {selected.length > 0 && (
+          <div className="flex flex-col justify-start">
+            {selected.map((sku) => (
+              <div
+                className="flex justify-between items-center mb-4"
+                key={sku.id}
+              >
+                <div className="flex-col">
+                  <div className="">{sku.product.name}</div>
+                  <div className="text-xs">
+                    {sku.size + " | " + sku.materialShort}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default SampleOrder;
--- pages/sample/index.tsx
+++ pages/sample/index.tsx
@@ -1,8 +1,17 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import ReLogo from "../../components/form/re-logo";
+import SampleOrder from "../../components/sample/sampleOrder";
+import prisma from "../../constants/prisma";
+import { SkuProduct } from "../../utils/dashboard/dashboardUtils";
+import { authOptions } from "../api/auth/[...nextauth]";
+import { unstable_getServerSession } from "next-auth";
 
-const SampleHome: NextPage = () => {
+type HomeProps = {
+  skus: SkuProduct[];
+};
+
+const Home: NextPage<HomeProps> = ({ skus }: HomeProps) => {
   return (
     <div className="w-screen h-screen bg-black flex items-center justify-center text-white">
       <Head>
@@ -15,9 +24,31 @@ const SampleHome: NextPage = () => {
         <div className="text-white font-theinhardt text-28">
           Welcome to sample page
         </div>
+        <SampleOrder skus={skus} />
       </main>
     </div>
   );
 };
 
-export default SampleHome;
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await unstable_getServerSession(
+    context.req,
+    context.res,
+    authOptions
+  );
+  if (session) {
+    const skus = await prisma.sku.findMany({
+      include: {
+        product: true,
+      },
+    });
+    return {
+      props: {
+        skus: JSON.parse(JSON.stringify(skus)),
+      },
+    };
+  }
+  return { props: {} };
+};
+
+export default Home;
