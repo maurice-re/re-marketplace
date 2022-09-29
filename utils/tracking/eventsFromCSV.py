4d529a26cf1a26f7c95feb1908cc9089c53b3d66
SuhanaNadeem <suhana.nadeemv@gmail.com>
2022-09-29 16:12:28 -0400

    figure out timing

--- utils/tracking/eventsFromCSV.py
+++ utils/tracking/eventsFromCSV.py
@@ -1,5 +1,7 @@
+from sqlite3 import Timestamp
 import requests
 import json
+import datetime
 
 # CSV location == API locationId == DB trackingLocation in Event
 
@@ -12,7 +14,17 @@ productSkuIdPairs = {"Food Container (M)":"SB1-1.5-RPP-GRAY", "Food Box (M)":"SB
 # response = requests.post(api_url, json=api_key)
 
 api_url = "http://localhost:3000/api/tracking/event"
-event = {"itemId": "3458b35d-e1e8-45b8-9197-468821dbee56", "locationId": "SSP-CPS", "skuId": "SB1-1-RPP-GRAY", "action": "RETURN"}
+date_handler = lambda obj: (
+    obj.isoformat()
+    if isinstance(obj, (datetime.datetime, datetime.date))
+    else None
+)
+
+# To get timestamp with 00:00:00
+# timestamp = datetime.datetime.strptime("May 25, 2022", "%B %d, %Y").strftime("%Y-%m-%d")
+# timestamp = timestamp[:10] + 'T' + '00:00:00Z'
+
+event = {"itemId": "0dff7feb-f0fc-4893-bba3-b76e207c4c4b", "locationId": "SSP-CPS", "skuId": "SB1-1-RPP-GRAY", "action": "BORROW", "timestamp": "May 25, 2022"}
 headers = {"content-type":"application/json", "authorization": "Bearer NrL3uRqdnUsbUZs1iJbhKiYrPqS6k1RXuXWBRwjlcgyYTjxA74n3lLuySOcWUg3u"}
 response = requests.post(api_url, data=json.dumps(event), headers=headers)
 
