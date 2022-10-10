a23b91dd62ca43c4f1d08e9e4b2500e2b48e6d43
maurice-re <107324227+maurice-re@users.noreply.github.com>
2022-10-10 13:48:44 -0400

    add avantika to quotes

--- pages/api/mail/send-quote.ts
+++ pages/api/mail/send-quote.ts
@@ -14,7 +14,7 @@ export default async function handler(req: Request, res: Response) {
   });
   const email = {
     from: "Maurice @ RE",
-    to: "maurice@re.company, maddie@re.company, matt@re.company",
+    to: "maurice@re.company, maddie@re.company, matt@re.company, avantika@re.company",
     subject: "Product Matching Quote Requested",
     text: message,
   };
