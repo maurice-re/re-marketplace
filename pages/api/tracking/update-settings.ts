d95b1277dd85824ab1ea25084df210b84216cf4a
SuhanaNadeem <suhana.nadeemv@gmail.com>
2022-10-24 09:44:47 -0400

    start update-settings

---
+++ pages/api/tracking/update-settings.ts
@@ -0,0 +1,16 @@
+import type { Request, Response } from "express";
+import prisma from "../../../constants/prisma";
+import { logApi } from "../../../utils/api/logging";
+
+async function updateSettings(req: Request, res: Response) {
+  const { companyId, borrowReturnBuffer }: { companyId: string, borrowReturnBuffer: number; } = req.body;
+  if (req.method == "POST" && typeof companyId == "string" && borrowReturnBuffer && borrowReturnBuffer > 0) {
+    console.log("In post with ", companyId, ' ', borrowReturnBuffer);
+    res.status(200).send({ success: `Successfully updated tracking settings for ${companyId} ` });
+  } else {
+    await logApi(`${req.method} event`, false, "HTTP Operation not supported");
+    res.status(401).send("Bad Request");
+  }
+}
+
+export default updateSettings;
