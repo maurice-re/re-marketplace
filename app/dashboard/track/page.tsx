42d7a58874d00b1e882bff2c38c792096a378b9f
SuhanaNadeem <suhana.nadeemv@gmail.com>
2022-11-03 09:27:59 -0400

    restructure app pt. 1

--- app/dashboard/track/page.tsx
+++ app/dashboard/track/page.tsx
@@ -1,21 +1,21 @@
-import { Action, Company, Event, Settings, Sku, User } from '@prisma/client';
-import { unstable_getServerSession } from 'next-auth';
-import Head from 'next/head';
-import { ChangeEvent, use } from 'react';
-import { Line } from 'react-chartjs-2';
-import prisma from '../../../constants/prisma';
-import {
-  UserWithSettings,
-} from '../../../utils/tracking/trackingUtils';
-import { authOptions } from '../../../pages/api/auth/[...nextauth]';
-import useSWR from 'swr';
-import TrackingChart from '../../trackingChart';
+import { Action, Company, Event, Settings, Sku, User } from '@prisma/client'
+import { unstable_getServerSession } from 'next-auth'
+import Head from 'next/head'
+import { ChangeEvent, use } from 'react'
+import { Line } from 'react-chartjs-2'
+import prisma from '../../../constants/prisma'
+import { UserWithSettings } from '../../../utils/tracking/trackingUtils'
+import { authOptions } from '../../../pages/api/auth/[...nextauth]'
+import useSWR from 'swr'
+import TrackingContent from '../../components/dashboard/tracking/trackingContent'
 
 async function getSkus() {
-  const skus = await prisma.sku.findMany();
-  return skus;
-};
-export type UserSettings = (User & { company: Company & { settings: Settings | null; }; }) | null;
+  const skus = await prisma.sku.findMany()
+  return skus
+}
+export type UserSettings =
+  | (User & { company: Company & { settings: Settings | null } })
+  | null
 
 async function getUser() {
   const user = await prisma.user.findUnique({
@@ -29,15 +29,25 @@ async function getUser() {
         },
       },
     },
-  });
-  return user;
+  })
+  return user
 }
 
 export default function Page() {
-
-  const skus = use(getSkus());
-  const user: UserSettings = use(getUser());
+  const skus = use(getSkus())
+  const user: UserSettings = use(getUser())
   // const user = use(getUser(context))
 
-  return (<div className="w-full"><TrackingChart user={user} skus={skus} /></div>);
-};
+  return (
+    <div className="w-full h-screen bg-black flex overflow-auto">
+      <Head>
+        <title>Tracking</title>
+        <meta name="tracking" content="Tracking" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex flex-col container mx-auto py-6 text-white font-theinhardt">
+        <TrackingContent user={user} skus={skus} />
+      </main>
+    </div>
+  )
+}
