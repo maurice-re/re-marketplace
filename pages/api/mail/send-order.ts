34b0ece24f1f77785ab1ab8d3a1e3c2fcb18d76f
maurice-re <107324227+maurice-re@users.noreply.github.com>
2022-12-14 22:02:52 -0500

    fix checkout and send email

---
+++ pages/api/mail/send-order.ts
@@ -0,0 +1,22 @@
+import type { Request, Response } from "express";
+import nodemailer from "nodemailer";
+
+
+export default async function handler(req: Request, res: Response) {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "maurice@re.company",
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+  const email = {
+    from: "Re Plaform",
+    to: "maurice@re.company, maddie@re.company, matt@re.company, avantika@re.company, suhana@re.company",
+    subject: "A new order was placed!",
+    text: "Check retool for more details https://recompany.retool.com/apps/2f02a446-5301-11ed-8842-374317c263f0/Orders",
+  };
+  await transporter.sendMail(email).catch(e => console.log(e));
+
+  res.send();
+}
