de2736a932c0d098d6c2d8797e7862e11e5baef0
maurice-re <107324227+maurice-re@users.noreply.github.com>
2023-01-23 11:21:16 -0500

    add hardware to location

---
+++ pages/api/hardware.ts
@@ -0,0 +1,38 @@
+import type { Request, Response } from "express";
+import { ApiError } from "next/dist/server/api-utils";
+import prisma from "../../constants/prisma";
+
+async function handler(req: Request, res: Response) {
+  if (req.method == "POST") {
+    const {
+      deviceId,
+      locationId,
+      action,
+      notes,
+    }: {
+      deviceId: string;
+      locationId: string;
+      action: string;
+      notes: string;
+    } = req.body;
+
+    try {
+      const newHardware = await prisma.hardware.create({
+        data: {
+          id: deviceId,
+          locationId: locationId,
+          return: action == "return",
+          notes: notes,
+        },
+      });
+      res.status(200).send({ id: newHardware.id });
+    } catch (e) {
+      const error = e as ApiError;
+      res
+        .status(500)
+        .send({ error: "Error creating hardware " + error.message });
+      return;
+    }
+  }
+}
+export default handler;
