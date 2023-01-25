ffc669ae02619b3f8cb9cbba54e2922102de7fe0
maurice-re <107324227+maurice-re@users.noreply.github.com>
2023-01-25 18:31:54 -0500

    force dynamic on checkout

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { User } from "@prisma/client";
 import { unstable_getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
