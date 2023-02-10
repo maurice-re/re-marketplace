a5425bdb4227f0dca1c1ea1015cdf5c4d8c317dc
maurice-re <107324227+maurice-re@users.noreply.github.com>
2023-02-10 10:58:44 -0500

    add roi calculator

---
+++ app/roi/page.tsx
@@ -0,0 +1,205 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+
+type RoiFields = {
+  type: string;
+  population: number;
+  singleUse: number;
+  locations: number;
+};
+
+const college = {
+  type: "College",
+  population: 40000,
+  singleUse: 0.15,
+  locations: 1,
+};
+
+const corporate = {
+  type: "Corporate",
+  population: 1000,
+  singleUse: 0.15,
+  locations: 1,
+};
+const hospital = {
+  type: "Hospital",
+  population: 500,
+  singleUse: 0.15,
+  locations: 1,
+};
+
+const reuseCost = 1.5;
+const daysBetween = 7;
+const returnRate = 0.9;
+
+export default function Page() {
+  const [form, setForm] = useState<RoiFields>(college);
+  function getDailyUses() {
+    if (form.type == "College") {
+      return form.population / 50;
+    } else if (form.type == "Corporate") {
+      return form.population / 20;
+    }
+    return form.population / 5;
+  }
+
+  function getInitialCost() {
+    return getDailyUses() * (daysBetween + 3) * reuseCost;
+  }
+
+  function getMonthlySavings() {
+    const singleUse = getDailyUses() * 30 * form.singleUse;
+    const reuse =
+      getDailyUses() * (daysBetween + 3) * (1 - returnRate) * reuseCost;
+    return singleUse - reuse;
+  }
+
+  function getBreakEven() {
+    return Math.round(getInitialCost() / getMonthlySavings());
+  }
+
+  function getAnnualSavings() {
+    return getMonthlySavings() * 12 - getInitialCost();
+  }
+
+  function getThreeYearSavings() {
+    return getMonthlySavings() * 36 - getInitialCost();
+  }
+
+  return (
+    <div className="h-screen w-full bg-re-dark-green-500 flex flex-col font-theinhardt text-white">
+      <div className="flex w-full border-b border-re-gray-300 bg-re-black">
+        <h1 className="text-2xl pl-6 py-4">Re ROI Calculator</h1>
+      </div>
+      <div className="absolute right-6 top-[4.5rem] flex">
+        <Image
+          src={"/images/logo.png"}
+          height={54}
+          width={71.12}
+          alt={"Re Company Logo"}
+        />
+      </div>
+      <div className="flex justify-center items-center h-full gap-4">
+        <div className="bg-re-dark-green-300 border border-re-gray-300 rounded-md">
+          <h3 className="py-2 px-4 text-lg">Inputs</h3>
+          <div className="w-full h-px bg-re-gray-300" />
+          <div className="flex flex-col px-4 py-2 gap-2">
+            <label className="text-re-gray-text">Business type</label>
+            <div className="flex w-full justify-between mb-1">
+              <button
+                id="college"
+                className={`px-3 py-1 bg-re-black border hover:border-re-green-500 active:border-re-green-700 rounded ${
+                  form.type == "College"
+                    ? "border-re-green-500"
+                    : "border-re-dark-green-300"
+                }`}
+                onClick={() => setForm(college)}
+              >
+                College
+              </button>
+              <button
+                id="corp"
+                className={`px-3 py-1 bg-re-black border hover:border-re-green-500 active:border-re-green-700 rounded ${
+                  form.type == "Corporate"
+                    ? "border-re-green-500"
+                    : "border-re-dark-green-300"
+                }`}
+                onClick={() => setForm(corporate)}
+              >
+                Corporate
+              </button>
+              <button
+                id="hosp"
+                className={`px-3 py-1 bg-re-black border hover:border-re-green-500 active:border-re-green-700 rounded ${
+                  form.type == "Hospital"
+                    ? "border-re-green-500"
+                    : "border-re-dark-green-300"
+                }`}
+                onClick={() => setForm(hospital)}
+              >
+                Hospital
+              </button>
+            </div>
+            <label className="text-re-gray-text mb-1">
+              Population
+              <input
+                type="number"
+                value={form.population}
+                className="rounded bg-re-black bg-opacity-[0.32] text-white w-full p-2 border-re-gray-300 border"
+                onChange={(e) =>
+                  setForm({ ...form, population: parseInt(e.target.value) })
+                }
+              ></input>
+            </label>
+            <label className="text-re-gray-text mb-1">
+              Cost of Single Use ($)
+              <input
+                type="number"
+                step="0.01"
+                value={form.singleUse}
+                className="rounded bg-re-black bg-opacity-[0.32] text-white w-full p-2 border-re-gray-300 border"
+                onChange={(e) =>
+                  setForm({ ...form, singleUse: parseFloat(e.target.value) })
+                }
+              ></input>
+            </label>
+            <label className="text-re-gray-text mb-1">
+              Number of Locations
+              <input
+                type="number"
+                value={form.locations}
+                className="rounded bg-re-black bg-opacity-[0.32] text-white w-full p-2 border-re-gray-300 border"
+                onChange={(e) =>
+                  setForm({ ...form, locations: parseInt(e.target.value) })
+                }
+              ></input>
+            </label>
+          </div>
+        </div>
+        <div className="bg-re-dark-green-300 border border-re-gray-300 rounded-md">
+          <h3 className="py-2 px-4 text-lg">Savings</h3>
+          <div className="w-full h-px bg-re-gray-300" />
+          <div className="flex flex-col px-4 py-2 gap-2">
+            <label className="text-re-gray-text mb-1 gap-3 flex justify-between">
+              Initial Investment:
+              <p className="text-white">
+                $
+                {getInitialCost().toLocaleString(undefined, {
+                  minimumFractionDigits: 2,
+                })}
+              </p>
+            </label>
+            <label className="text-re-gray-text mb-1 gap-3 flex justify-between">
+              Monthly Savings:
+              <p className="text-re-green-500 text-lg">
+                $
+                {getMonthlySavings().toLocaleString(undefined, {
+                  minimumFractionDigits: 2,
+                })}
+              </p>
+            </label>
+            <label className="text-re-gray-text mb-1 gap-3 flex justify-between">
+              Breakeven:
+              <p className="text-re-green-500 text-lg">
+                {getBreakEven()} months
+              </p>
+            </label>
+            <label className="text-re-gray-text mb-1 gap-3 flex justify-between">
+              1 year savings:
+              <p className="text-re-green-500 text-lg">
+                ${getAnnualSavings().toLocaleString()}
+              </p>
+            </label>
+            <label className="text-re-gray-text mb-1 gap-3 flex justify-between">
+              3 year savings:
+              <p className="text-re-green-500 text-lg">
+                ${getThreeYearSavings().toLocaleString()}
+              </p>
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
