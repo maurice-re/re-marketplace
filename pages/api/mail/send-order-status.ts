27c4bf23a507f064f5e379d72ea8cc2cf9790037
SuhanaNadeem <suhana.nadeemv@gmail.com>
2023-02-11 12:13:10 -0800

    create api endpoint for retool

---
+++ pages/api/mail/send-order-status.ts
@@ -0,0 +1,23 @@
+import type { Request, Response } from "express";
+import nodemailer from "nodemailer";
+
+// TODO(Suhana): Combine all emailers into one function
+export default async function handler(req: Request, res: Response) {
+    const { to, orderId, status } = req.body;
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "maurice@re.company",
+            pass: process.env.EMAIL_PASS,
+        },
+    });
+    const email = {
+        from: "Re Platform",
+        to: to,
+        subject: "Update to Your Order",
+        text: "Order #" + orderId + " is now " + status + ".",
+    };
+    await transporter.sendMail(email).catch(e => console.log(e));
+
+    res.send();
+}
