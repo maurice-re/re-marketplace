e3365a4ce593d75e4adce60988d34522450d63cb
maurice-re <107324227+maurice-re@users.noreply.github.com>
2023-03-24 16:54:48 -0400

    remove location until ready

--- app/dashboard/tracking/page.tsx
+++ app/dashboard/tracking/page.tsx
@@ -7,6 +7,22 @@ export default async function Page() {
 
   const locations = [...ownedLocations, ...viewableLocations];
 
+  const hasEvents = locations.some((location) => location.events.length > 0);
+  if (!hasEvents) {
+    return (
+      <div className="w-full h-screen bg-re-dark-green-500 flex overflow-auto px-6">
+        {/* <head>
+          <title>Tracking</title>
+          <meta name="tracking" content="Tracking" />
+          <link rel="icon" href="/favicon.ico" />
+        </head> */}
+        <main className="flex flex-col container mx-auto py-6 text-white font-theinhardt">
+          <div>Integrate with our API and see your system KPIs</div>
+        </main>
+      </div>
+    );
+  }
+
   return (
     <div className="w-full h-screen bg-re-dark-green-500 flex overflow-auto px-6">
       {/* <head>
@@ -15,10 +31,7 @@ export default async function Page() {
         <link rel="icon" href="/favicon.ico" />
       </head> */}
       <main className="flex flex-col container mx-auto py-6 text-white font-theinhardt">
-        <Tracking
-          demo={false}
-          locations={locations}
-        />
+        <Tracking demo={false} locations={locations} />
       </main>
     </div>
   );
