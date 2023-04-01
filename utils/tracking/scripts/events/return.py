e900f0cd52b1c7e50eeed843d36af9c87b9b3e45
SuhanaNadeem <suhana.nadeemv@gmail.com>
2023-04-01 15:15:11 -0700

    create .py tester for return endpoint

--- utils/tracking/scripts/events/return.py
+++ utils/tracking/scripts/events/return.py
@@ -4,7 +4,7 @@ import requests
 # npm run dev
 # Run using VSCode "Run" button
 
-apiUrl = "http://localhost:3000/api/tracking/borrow"
+apiUrl = "http://localhost:3000/api/tracking/return"
 
 body = {"consumerId": "test consumerId", "itemId": "test itemId", "hardwareId": "test hardwareId", "skuId": "test skuId", "timestamp": "test timestamp", "locationId": "test locationId"}
 headers = {"content-type":"application/json", "authorization": "Bearer DSwXumUE5eTQbYM3nI6qSPBWrtF0yp6IhjiXEVYvWMcahTyR0MWdUka1ywkWWaK8"}
