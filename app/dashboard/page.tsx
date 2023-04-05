e5f1234ac81d06a515387ab73a6967f12f7b2562
maurice-re <107324227+maurice-re@users.noreply.github.com>
2023-04-05 09:58:29 -0400

    extract components from home

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,34 +1,19 @@
-import { Company, Status, User } from "@prisma/client";
-import { FullLocation, FullOrder, useServerStore } from "../server-store";
-import Home from "./home";
+import { Company, User } from "@prisma/client";
+import { FullOrder, useServerStore } from "../server-store";
+import Home from "./home/home";
 
 export default async function Page() {
   const user: User = await useServerStore.getState().getUser();
-  const locations: FullLocation[] = await useServerStore.getState().getLocations(true);
   const orders: FullOrder[] = await useServerStore.getState().getOrders();
   const company = await useServerStore.getState().getCompany();
   const skus = await useServerStore.getState().getSkus();
 
-  const completeOrders = orders.filter(
-    (order: FullOrder) => order.status === Status.COMPLETED
-  );
-  const incompleteOrders = orders.filter(
-    (order: FullOrder) => order.status !== Status.COMPLETED
-  );
-
-  const hasCompleteOrder: boolean =
-    completeOrders.length > 0 || user.companyId === "616";
-  const hasIncompleteOrder: boolean = incompleteOrders.length > 0;
-
   return (
     <Home
       company={company ?? ({} as Company)}
-      locations={locations}
       orders={orders}
       user={user}
       skus={skus}
-      hasCompleteOrder={hasCompleteOrder}
-      hasIncompleteOrder={hasIncompleteOrder}
     />
   );
 }
