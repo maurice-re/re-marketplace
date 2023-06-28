d568f2a711b39cb6b2b32cdb5a3223f43fb30f9f
maurice-re <107324227+maurice-re@users.noreply.github.com>
2023-06-28 12:26:50 -0400

    moq for custom colors & switch to react based form

--- app/quote/page.tsx
+++ app/quote/page.tsx
@@ -3,6 +3,7 @@
 import { UploadDropzone } from "@uploadthing/react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import type { Dispatch, SetStateAction } from "react";
 import { useState } from "react";
 import ReLogo from "../../components/form/re-logo";
 import { OurFileRouter } from "../api/uploadthing/core";
@@ -21,69 +22,117 @@ const products: QuoteProduct[] = [
   { name: "1.5 L Swapbox", image: "/images/1_5L_swapbox_main.png" },
 ];
 
-const indexToName: Record<number, string> = {
-  0: "8oz Hot Cup",
-  4: "12oz Hot Cup",
-  8: "16oz Hot Cup",
-  12: "20oz Cold Cup",
-  16: "1 L Swapbox",
-  20: "1.5 L Swapbox",
+type QuoteRequest = {
+  name: string;
+  image: string;
+  quantity: number;
+  color: string;
+  logo: boolean;
 };
 
-function ColorPicker() {
-  const [selectedColor, changeColor] = useState<string>("#6C7982");
+const colorToName: Record<string, string> = {
+  "#D3163B": "Red",
+  "#3D5FA0": "Dark Blue",
+  "#004645": "Green",
+  clear: "Clear",
+  "#6C7982": "Base Grey",
+  "#A4D5EE": "Light Blue",
+};
 
+function ColorPicker({
+  index,
+  quoteRequests,
+  updateRequest,
+}: {
+  index: number;
+  quoteRequests: QuoteRequest[];
+  updateRequest: Dispatch<SetStateAction<QuoteRequest[]>>;
+}): JSX.Element {
   return (
     <div className="flex w-full flex-col items-center text-base xl:text-lg">
       <div className="mb-2 grid grid-cols-3 gap-1 xl:gap-2">
         <div
           onClick={() => {
-            changeColor("#D3163B");
+            updateRequest((currRequests) => {
+              const newRequests = [...currRequests];
+              newRequests[index].color = "#D3163B";
+              return newRequests;
+            });
           }}
           className={`h-6 w-6 rounded bg-[#D3163B] hover:border hover:border-white xl:h-10 xl:w-10 ${
-            selectedColor == "#D3163B" ? "border border-re-green-500" : ""
+            quoteRequests[index].color == "#D3163B"
+              ? "border border-re-green-500"
+              : ""
           }`}
         ></div>
         <div
           onClick={() => {
-            changeColor("#3D5FA0");
+            updateRequest((currRequests) => {
+              const newRequests = [...currRequests];
+              newRequests[index].color = "#3D5FA0";
+              return newRequests;
+            });
           }}
           className={`h-6 w-6 rounded bg-[#3D5FA0] hover:border hover:border-white xl:h-10 xl:w-10 ${
-            selectedColor === "#3D5FA0" ? "border border-re-green-500" : ""
+            quoteRequests[index].color === "#3D5FA0"
+              ? "border border-re-green-500"
+              : ""
           }`}
         ></div>
         <div
           onClick={() => {
-            changeColor("#004645");
+            updateRequest((currRequests) => {
+              const newRequests = [...currRequests];
+              newRequests[index].color = "#004645";
+              return newRequests;
+            });
           }}
           className={`h-6 w-6 rounded bg-[#004645] hover:border hover:border-white xl:h-10 xl:w-10 ${
-            selectedColor === "#004645" ? "border border-re-green-500" : ""
+            quoteRequests[index].color === "#004645"
+              ? "border border-re-green-500"
+              : ""
           }`}
         ></div>
         <div
           onClick={() => {
-            changeColor("clear");
+            updateRequest((currRequests) => {
+              const newRequests = [...currRequests];
+              newRequests[index].color = "clear";
+              return newRequests;
+            });
           }}
           className={`flex h-6 w-6 items-center justify-center rounded border border-black bg-transparent text-xs hover:border hover:border-white xl:h-10 xl:w-10 xl:text-sm ${
-            selectedColor === "clear" ? "border-re-green-500" : ""
+            quoteRequests[index].color === "clear" ? "border-re-green-500" : ""
           }`}
         >
           clear
         </div>
         <div
           onClick={() => {
-            changeColor("#6C7982");
+            updateRequest((currRequests) => {
+              const newRequests = [...currRequests];
+              newRequests[index].color = "#6C7982";
+              return newRequests;
+            });
           }}
           className={`h-6 w-6 rounded bg-[#6C7982] hover:border hover:border-white xl:h-10 xl:w-10 ${
-            selectedColor === "#6C7982" ? "border border-re-green-500" : ""
+            quoteRequests[index].color === "#6C7982"
+              ? "border border-re-green-500"
+              : ""
           }`}
         ></div>
         <div
           onClick={() => {
-            changeColor("#A4D5EE");
+            updateRequest((currRequests) => {
+              const newRequests = [...currRequests];
+              newRequests[index].color = "#A4D5EE";
+              return newRequests;
+            });
           }}
           className={`h-6 w-6 rounded bg-[#A4D5EE] hover:border hover:border-white xl:h-10 xl:w-10 ${
-            selectedColor === "#A4D5EE" ? "border border-re-green-500" : ""
+            quoteRequests[index].color === "#A4D5EE"
+              ? "border border-re-green-500"
+              : ""
           }`}
         ></div>
       </div>
@@ -91,43 +140,61 @@ function ColorPicker() {
         className={`w-3/5 rounded bg-re-dark-green-500 py-1 pl-2 text-white`}
         type="text"
         placeholder="Hex Code"
-        value={selectedColor}
+        value={quoteRequests[index].color}
         onChange={(event) => {
-          changeColor(event.target.value);
+          updateRequest((currRequests) => {
+            const newRequests = [...currRequests];
+            newRequests[index].color = event.target.value;
+            return newRequests;
+          });
         }}
       />
+      {quoteRequests[index].color != "#6C7982" && (
+        <p className="mt-1 text-xs text-error">
+          MOQ for this color is 3,000 units
+        </p>
+      )}
     </div>
   );
 }
 
 export default function Page() {
   const router = useRouter();
+  const [quoteRequests, updateRequest] = useState<QuoteRequest[]>(
+    products.reduce<QuoteRequest[]>((requests, product) => {
+      requests.push({
+        name: product.name,
+        image: product.image,
+        quantity: 0,
+        color: "#6C7982",
+        logo: false,
+      });
+      return requests;
+    }, [])
+  );
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
 
   async function handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    const formElements = (event.target as any).elements as HTMLInputElement[];
-    console.log(formElements);
-    const quantityIndexes = [0, 4, 8, 12, 16, 20];
+    setLoading(true);
+
+    // Send Email
     let quoteRequested = "";
-    for (let i = 0; i < quantityIndexes.length; i++) {
-      let quantity = formElements[quantityIndexes[i]].value;
-      quantity = quantity.replace(",", "");
-      const quantityNumber = parseInt(quantity);
+    console.log(quoteRequests);
+    quoteRequests.forEach((request) => {
+      const quantity = request.quantity;
+      const quantityNumber = quantity;
       console.log(quantityNumber);
-      if (isNaN(quantityNumber)) {
-        continue;
-      } else if (quantityNumber <= 0) {
-        continue;
-      } else {
-        const name = indexToName[quantityIndexes[i]];
-        const quantity = quantityNumber;
-        const color = formElements[quantityIndexes[i] + 1].value;
-        const logo = formElements[quantityIndexes[i] + 2].checked
-          ? "with Logo"
-          : "without Logo";
-        quoteRequested += `${name}, ${quantity} units, color code: ${color}, ${logo} \n`;
+      if (quantityNumber > 0) {
+        const logo = request.logo ? "with Logo" : "without Logo";
+        quoteRequested += `${request.name}, ${quantity} units, color: ${
+          colorToName[request.color]
+            ? colorToName[request.color]
+            : "custom color code: " + request.color
+        }, ${logo} \n`;
       }
-    }
+    });
 
     await fetch("/api/mail/send-quote", {
       method: "POST",
@@ -136,16 +203,25 @@ export default function Page() {
       },
       body: JSON.stringify({
         message: quoteRequested,
-        email: formElements[24].value,
+        email: email,
       }),
     });
 
     router.push("/quote/success");
   }
+
+  function minQuantity(quoteReq: QuoteRequest): number {
+    if (quoteReq.quantity == 0) {
+      return 0;
+    } else if (quoteReq.quantity < 3000 && quoteReq.color != "#6C7982") {
+      return 3000;
+    }
+    return 500;
+  }
   return (
     <div className="flex h-screen w-full flex-col overflow-auto bg-re-black font-theinhardt text-white">
       <ReLogo />
-      <div className="container mx-auto my-10 w-1/2">
+      <div className="container mx-auto my-10 w-3/5">
         <h1
           className="mb-6 bg-gradient-to-r from-white
         to-[#58FEC467] bg-clip-text text-center text-7xl text-transparent"
@@ -172,51 +248,69 @@ export default function Page() {
               </tr>
             </thead>
             <tbody className="divide-y divide-re-gray-300">
-              {products.map((product) => {
+              {quoteRequests.map((request, index) => {
                 return (
                   <tr
-                    key={product.name}
+                    key={request.name}
                     className="divide-x divide-re-gray-300 text-center text-base odd:bg-re-table-odd even:bg-re-table-even xl:text-lg"
                   >
                     <td>
                       <Image
-                        src={product.image}
-                        alt={product.name}
+                        src={request.image}
+                        alt={request.name}
                         width={120}
                         height={120}
-                        className="p-2"
+                        className="mx-auto bg-blue-300"
                       />
                     </td>
-                    <td className="px-2">{product.name}</td>
+                    <td className="px-2">{request.name}</td>
                     <td>
                       <input
                         className="w-24 rounded bg-re-dark-green-500 py-1 pl-2 text-white"
-                        type="text"
-                        placeholder="0"
-                        onInput={(event) => {
-                          const inputElement = event.target as HTMLInputElement;
-                          const value = inputElement.value;
-                          // Remove existing commas from the value
-                          const sanitizedValue = value.replace(/,/g, "");
-                          // Convert the sanitized value to a number
-                          const numberValue = Number(sanitizedValue);
-                          // Check if the number is valid
-                          if (!isNaN(numberValue)) {
-                            // Format the number with commas
-                            const formattedValue = numberValue.toLocaleString();
-                            // Set the formatted value back to the input
-                            inputElement.value = formattedValue;
-                          }
+                        type="number"
+                        value={parseInt("" + request.quantity)}
+                        min={minQuantity(request)}
+                        onChange={(event) => {
+                          updateRequest((currRequest) => {
+                            const newRequests = [...currRequest];
+                            newRequests[index] = {
+                              ...newRequests[index],
+                              quantity:
+                                event.target.value == ""
+                                  ? 0
+                                  : parseInt(event.target.value, 10),
+                            };
+                            return newRequests;
+                          });
                         }}
                       />
                     </td>
                     <td className="py-2">
-                      <ColorPicker />
+                      <ColorPicker
+                        index={index}
+                        quoteRequests={quoteRequests}
+                        updateRequest={updateRequest}
+                      />
                     </td>
                     <td>
                       <input
                         type="checkbox"
                         className="mr-2 bg-re-dark-green-500"
+                        value={request.logo ? 1 : 0}
+                        onChange={(event) => {
+                          updateRequest((currRequest) => {
+                            const index = currRequest.findIndex(
+                              (curr) => curr.name === request.name
+                            );
+                            if (index < 0) return currRequest;
+                            const newRequests = [...currRequest];
+                            newRequests[index] = {
+                              ...newRequests[index],
+                              logo: event.target.checked,
+                            };
+                            return newRequests;
+                          });
+                        }}
                       />
                       <label>Logo (pad printing)</label>
                     </td>
@@ -234,11 +328,25 @@ export default function Page() {
         <div className="mx-auto my-10 flex w-3/5 justify-between text-xl">
           <input
             className="w-3/5 rounded border border-re-gray-300 bg-re-dark-green-500 py-2 pl-4 text-white"
+            value={email}
             type="text"
             placeholder="Email(s) for quote to be sent to"
             required
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
           />
-          <button className="w-1/5 rounded-md bg-re-purple-500">Submit</button>
+          <button
+            className="flex w-1/5 items-center justify-center rounded-md bg-re-purple-500 disabled:bg-re-gray-300"
+            disabled={quoteRequests.every(
+              (request) => request.quantity === 0 || isNaN(request.quantity)
+            )}
+          >
+            Submit
+            {loading && (
+              <span className="loading loading-spinner loading-xs ml-4"></span>
+            )}
+          </button>
         </div>
       </form>
     </div>
